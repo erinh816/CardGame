9820bb7a283587c26d3995246c081d20afafa0ad
Silu Hu <erinh816@gmail.com>
2021-09-27 22:36:49 -0400

    basic cards

---
+++ card-game/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+// import Pokecard from "./Pokecard";
+import Pokedex from "./Pokedex";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Pokecard id={4} name="Charmander" type="fire" exp={62} /> */}
+      <Pokedex />
+    </div>
+  );
+}
+
+export default App;
