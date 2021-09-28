0123f1f76bb9d341311ecc5c7afa903a24f4a871
Silu Hu <erinh816@gmail.com>
2021-09-28 08:11:42 -0400

    using the fancy image API but id number logic I don't understand

--- card-game/src/Pokecard.js
+++ card-game/src/Pokecard.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import "./Pokecard.css";
-const POKE_API =
-  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+// const POKE_API =
+//   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+
+//fancier img API, but ID has to be modified
+const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
+
+let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
 
 export default class Pokecard extends Component {
   render() {
-    let imgSrc = `${POKE_API}${this.props.id}.png`;
+    // let imgSrc = `${POKE_API}${this.props.id}.png`;
 
+    let imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`;
     return (
       <div className="Pokecard">
         <h1 className="Pokecard-title">{this.props.name}</h1>
