44a7b54d2ed5d62d9215d62a9cbc5a39c45d2a6e
Silu Hu <erinh816@gmail.com>
2021-10-03 19:06:33 -0400

    add class for winner and loser

--- card-game/src/Pokedex.js
+++ card-game/src/Pokedex.js
@@ -3,27 +3,37 @@ import Pokecard from "./Pokecard";
 import "./Pokedex.css";
 
 class Pokedex extends Component {
-  static defaultProps = {
-    pokemon: [
-      { id: 4, name: "Charmander", type: "fire", base_experience: 62 },
-      { id: 7, name: "Squirtle", type: "water", base_experience: 63 },
-      { id: 11, name: "Metapod", type: "bug", base_experience: 72 },
-      { id: 12, name: "Butterfree", type: "flying", base_experience: 178 },
-      { id: 25, name: "Pikachu", type: "electric", base_experience: 112 },
-      { id: 39, name: "Jigglypuff", type: "normal", base_experience: 95 },
-      { id: 94, name: "Gengar", type: "poison", base_experience: 225 },
-      { id: 133, name: "Eevee", type: "normal", base_experience: 65 },
-    ],
-  };
+  // static defaultProps = {
+  //   pokemon: [
+  //     { id: 4, name: "Charmander", type: "fire", base_experience: 62 },
+  //     { id: 7, name: "Squirtle", type: "water", base_experience: 63 },
+  //     { id: 11, name: "Metapod", type: "bug", base_experience: 72 },
+  //     { id: 12, name: "Butterfree", type: "flying", base_experience: 178 },
+  //     { id: 25, name: "Pikachu", type: "electric", base_experience: 112 },
+  //     { id: 39, name: "Jigglypuff", type: "normal", base_experience: 95 },
+  //     { id: 94, name: "Gengar", type: "poison", base_experience: 225 },
+  //     { id: 133, name: "Eevee", type: "normal", base_experience: 65 },
+  //   ],
+  // };
+  //get rid of above because Pokegame.js manipulate it directly
   render() {
+    let title;
+    if (this.props.isWinner) {
+      title = <h1 className="Pokedex-winner">Winning Hand</h1>;
+    } else {
+      title = <h1 className="Pokedex-loser">Losing Hand</h1>;
+    }
     return (
       <div className="Pokedex">
         <h1>Pokedex!</h1>
-
+        {title}
         {/* total exp */}
-        <p>Total Exp {this.props.exp}</p>
+        <h4>Total Exp {this.props.exp}</h4>
 
-        <p>{this.props.isWinner ? "Winner" : "Loser"}</p>
+        {/* we can use logic on className to show different style of winner and loser */}
+        {/* <p className={this.props.isWinner ? "Pokedex-winner" : "Pokedex=loser"}>
+          {this.props.isWinner ? "Winner" : "Loser"}
+        </p> */}
 
         <div className="Pokedex-cards">
           {this.props.pokemon.map((p) => (
